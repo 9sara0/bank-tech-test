e277a92270751419b0e026585f05c5a774d16fcb
Sara <sarah.alalaya@gmail.com>
2016-11-29 13:23:08 +0300

    Transaction: it saves credited amount

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,11 +1,16 @@
 require "transaction"
 
 describe Transaction do
-  subject(:transaction) { described_class.new }
+  let(:credit)          { double :credit }
+  subject(:transaction) { described_class.new(credit) }
 
   context "When initialized" do
     it "knows the transaction's date" do
       expect(transaction.date).to eq Date.today
+    end
+
+    it "saves the credited amount" do
+      expect(transaction.credit).to eq credit
     end
   end
 
