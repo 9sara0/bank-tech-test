b467a17a14aff89491614ee03c08159b4175eec7
Sara <sarah.alalaya@gmail.com>
2016-11-29 18:51:44 +0300

    Transaction: it saves the new balance

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,11 +1,12 @@
 require "date"
 
 class Transaction
-  attr_reader :date, :credit, :debit
+  attr_reader :date, :credit, :debit, :balance
 
-  def initialize(credit,debit)
-    @date   = Date.today
-    @credit = credit
-    @debit  = debit
+  def initialize(credit,debit, balance)
+    @date    = Date.today
+    @credit  = credit
+    @debit   = debit
+    @balance = balance
   end
 end
