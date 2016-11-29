f479886f64f906b407be823fa758aa84096a00fe
Sara <sarah.alalaya@gmail.com>
2016-11-29 18:43:59 +0300

    Adds Transaction class that saves the date of a transaction

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,11 @@
+require "transaction"
+
+describe Transaction do
+  subject(:transaction) { described_class.new }
+
+  context "When initialized" do
+    it "knows the transaction's date" do
+      expect(transaction.date).to eq Date.today
+    end
+  end
+end
