5bb29f86746b1b9a1f009be8fe8c5dcca1010e99
Sara <sarah.alalaya@gmail.com>
2016-11-29 18:58:05 +0300

    Transaction: its default credit = 0

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -4,7 +4,7 @@ describe Transaction do
   let(:credit)          { double :credit }
   let(:debit)           { double :debit }
   let(:balance)         { double :balance }
-  subject(:transaction) { described_class.new(credit, debit, balance) }
+  subject(:transaction) { described_class.new(credit: credit, debit: debit, balance: balance) }
 
   context "When initialized" do
     it "knows the transaction's date" do
@@ -21,6 +21,11 @@ describe Transaction do
 
     it "saves the balance after the transaction is preformed" do
       expect(transaction.balance).to eq balance
+    end
+
+    it "has a default credit of zero" do
+      transaction = described_class.new(debit: debit, balance: balance)
+      expect(transaction.credit).to eq 0
     end
   end
 end
